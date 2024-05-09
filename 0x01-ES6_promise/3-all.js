df3eca7a7f72192ef1814418aff209f0dae083f1
Samir Idris <samzidris11@gmail.com>
2024-05-09 14:40:51 +0000

    add task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignUp() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => console.log(`${res[0].body} \
+${res[1].firstName} ${res[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
