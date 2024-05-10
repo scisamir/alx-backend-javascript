30c447407ec140a79714c9377f7d8166209b7d69
Samir Idris <samzidris11@gmail.com>
2024-05-10 18:32:20 +0000

    modify task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -8,31 +12,24 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(val) {
-    if (typeof val === 'string') {
-      this._name = val;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
+    this._name = val;
   }
 
   get length() {
     return this._length;
   }
+
   set length(val) {
-    if (typeof val === 'number') {
-      this._length = val;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
+    this._length = val;
   }
 
   get students() {
     return this._students;
   }
+
   set students(val) {
-    if (val.isArray()) {
-      this._students = val;
-    }
+    this._students = val;
   }
 }
