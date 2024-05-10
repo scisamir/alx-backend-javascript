d70641461567367e70582348b07618395eeee2ee
Samir Idris <samzidris11@gmail.com>
2024-05-10 13:43:20 +0000

    modify task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,19 +3,17 @@ import uploadUser from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName),
-      uploadUser(filename)])
+    uploadUser(filename)])
     .then((result) => {
       const resArray = [];
-      console.log(result);
 
       result.forEach((res) => {
         if (Object.keys(res)[1] !== 'reason') {
-	  resArray.push({ status: res.status, value: res.value });
-	} else {
-	  resArray.push({ status: res.status, value: `${res.reason}` });
-	}
+          resArray.push({ status: res.status, value: res.value });
+        } else {
+          resArray.push({ status: res.status, value: `${res.reason}` });
+        }
       });
-      console.log(resArray);
       return resArray;
     });
 }
