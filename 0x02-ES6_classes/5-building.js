1e9ff69d8c2967b8bf3cef80998740a64d400f80
Samir Idris <samzidris11@gmail.com>
2024-05-11 22:42:18 +0000

    modify task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
   constructor(sqft) {
+    if (this.evacuationWarningMessage === undefined && new.target !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
