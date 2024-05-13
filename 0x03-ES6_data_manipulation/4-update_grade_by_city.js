dbb01e381f3fc5e170db00a05a7873f3d522e44b
Samir Idris <samzidris11@gmail.com>
2024-05-13 22:01:42 +0000

    fix lint: no parameter reassignment

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,16 @@
 export default function updateStudentGradeByCity(arr, city, newGds) {
   return arr.map((item) => {
     let gradeCheck = 0;
+    let modifiedItem = {};
 
     for (let i = 0; i < newGds.length; i += 1) {
       if (item.id === newGds[i].studentId) {
-        const modifiedItem = { ...item, grade: newGds[i].grade };
+        modifiedItem = { ...item, grade: newGds[i].grade };
         gradeCheck = 1;
         break;
       }
       if (gradeCheck === 0 && (i + 1 === newGds.length)) {
-        const modifiedItem = { ...item, grade: 'N/A' };
+        modifiedItem = { ...item, grade: 'N/A' };
       }
     }
 
