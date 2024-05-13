226e46967a2d4b194b00157a8d4d4f8193b74b61
Samir Idris <samzidris11@gmail.com>
2024-05-13 21:50:41 +0000

    modify task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,18 @@
 export default function updateStudentGradeByCity(arr, city, newGds) {
-  arr.map((item) => {
+  return arr.map((item) => {
     let gradeCheck = 0;
 
-    for (let i = 0; i < newGds.length; i++) {
+    for (let i = 0; i < newGds.length; i += 1) {
       if (item.id === newGds[i].studentId) {
-        item.grade = newGds[i].grade;
+        item = { ...item, grade: newGds[i].grade };
         gradeCheck = 1;
+        break;
       }
       if (gradeCheck === 0 && (i + 1 === newGds.length)) {
-        item.grade = 'N/A';
+        item = { ...item, grade: 'N/A' };
       }
     }
-  });
 
-  return arr.filter((item) => item.location === city);
+    return item;
+  }).filter((item) => item.location === city);
 }
