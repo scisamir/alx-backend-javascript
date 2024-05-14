e34fd0f78ac23e9b8ee3badbbbf9295797a6a586
Samir Idris <samzidris11@gmail.com>
2024-05-14 02:16:08 +0000

    add task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,16 @@
-export default function cleanSet(theSet, str) {
-  
+export default function cleanSet(set, startString) {
+  let resStr = '';
+
+  set.forEach((val) => {
+    if (val.startsWith(startString) && startString !== '') {
+      const subStr = val.substring(startString.length);
+      resStr += `${subStr}-`;
+    }
+  });
+
+  if (resStr !== '') {
+    resStr = resStr.substring(0, resStr.length - 1);
+  }
+
+  return resStr;
 }
