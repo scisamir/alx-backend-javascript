88b89924dce7e129b3f51a6326fb59a11f0d2394
Samir Idris <samzidris11@gmail.com>
2024-05-14 01:45:48 +0000

    add task 7

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,13 @@
+export default function hasValuesFromArray(theSet, anArr) {
+  let hasArr = true;
+
+  anArr.map((elem) => {
+    hasArr = theSet.has(elem);
+    if (hasArr === false) {
+      return;
+    }
+    return hasArr;
+  });
+
+  return hasArr;
+}
