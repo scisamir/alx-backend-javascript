02131a3ce75738e482880cdd9bf12f604ec08df0
Samir Idris <samzidris11@gmail.com>
2024-05-15 04:11:53 +0000

    add task 100; weakmap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(arg) {
+  if (!weakMap.has(arg)) {
+    weakMap.set(arg, 1);
+    return weakMap.get(arg);
+  }
+
+  let nQueries = weakMap.get(arg);
+  nQueries += 1;
+
+  if (nQueries >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(arg, nQueries);
+}
